97a8cf9ede485565612ab3595778f8b6bd4ead5f
MB-study <MB-study@192.168.1.72>
2020-08-11 16:04:40 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/test/Practice1.java
@@ -0,0 +1,15 @@
+package com.pcschool.ocp.test;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        int[] x = { 10,20,30};
+        int sum = 0;
+        for (int i = 0; i<x.length;i++){
+            sum += x[i];
+            
+        }
+            
+        System.out.printf("總和: %d",sum);
+    }
+    
+}
