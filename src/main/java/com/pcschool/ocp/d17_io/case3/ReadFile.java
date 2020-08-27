6595a209a97f330e186a1fb90c76b0e2fc67d030
MB-study <MB-study@192.168.1.72>
2020-08-27 14:00:09 +0800

    Java

--- src/main/java/com/pcschool/ocp/d17_io/case3/ReadFile.java
+++ src/main/java/com/pcschool/ocp/d17_io/case3/ReadFile.java
@@ -1,4 +1,3 @@
-  
 package com.pcschool.ocp.d17_io.case3;
 
 import java.io.File;
---
+++ src/main/java/com/pcschool/ocp/d18_nio/case1/case2/FindBug.java
@@ -0,0 +1,26 @@
+package com.pcschool.ocp.d18_nio.case1.case2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+
+
+public class FindBug {
+        public static void main(String[] args) throws Exception {
+        Path path = Paths.get("src\\main\\java\\com\\pcschool\\ocp\\d18_nio\\report.txt");
+        Optional<String> data = Files.readAllLines(path)
+                .stream()
+               .filter(s -> s.contains("bug"))
+                .parallel()     //多工
+                .findFirst();
+             if(data.isPresent()){
+                 System.out.println("找到了" + data.get());
+             }else{
+                 System.out.println("沒找到");
+             }
+    
+}
+}
